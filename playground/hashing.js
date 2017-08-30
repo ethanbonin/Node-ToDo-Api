82a7bcd85896b94e00e9637171ab278aba4256ad
ethanbonin <ethanbonin@gmail.com>
2017-08-30 08:17:35 -0600

    Added the hashing of passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var message = 'I am user number 3';
 var hash = SHA256(message).toString();
@@ -33,7 +34,6 @@ if (resultHash === token.hash){
 
 
 //---------JSONWEBTOKEN---------//
-
 var data = {
   id: 10
 };
@@ -41,7 +41,25 @@ var data = {
 //The data that gets hashed, then the signature.
 var token = jwt.sign(data, '123abc');
 var decoded = jwt.verify(token, '123abc');
-
-
 console.log(token);
 console.log(decoded);
+
+
+//---------BCRYPT-------------//
+var password = 'abc123'
+
+//10 of number of rounds,
+//number of rounds is how complex the salt is going to be.
+//Thus 120 rounds takes about---> GREATER THAN 5 MINUTES
+bcrypt.genSalt(10, (err, salt) => {
+  //(thing to hash, )
+  bcrypt.hash(password, salt, (err, hash) => {
+      console.log("hashed Password: ", hash);
+  });
+});
+
+
+var hashed_value= '$2a$10$7lHWis5wp81DAOUfgZrPzOHZdLndpE.9GAfJNSKf44HH9t2OOQjLO'
+bcrypt.compare(password, hashed_value, (err, response) => {
+    console.log(response);
+});
