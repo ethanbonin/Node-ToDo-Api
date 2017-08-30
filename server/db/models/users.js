b37a945406d6020752ffd981d07c6a147d3f77c3
ethanbonin <ethanbonin@gmail.com>
2017-08-30 17:28:15 -0600

    Added a DELETE /users/me/token

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -68,6 +68,23 @@ UserSchema.methods.generateAuthToken = function () {
 };
 
 
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {
+        token: token
+      }
+    }
+  });
+
+};
+
+
+
+
 //This is a model Method!
 UserSchema.statics.findByToken = function (token) {
   var User = this;
@@ -134,6 +151,8 @@ UserSchema.pre('save', function(next) {
     next();
   }
 });
+
+
 
 
 
